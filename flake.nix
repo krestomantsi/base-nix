9d64b9ce24080b61f540589a565ebc2b398ea4a0
Krestomantsi <86400115+krestomantsi@users.noreply.github.com>
2024-03-03 20:31:58 +0200

    Add files via upload

--- flake.nix
+++ flake.nix
@@ -1,110 +1,29 @@
 {
-  description = "Josh's NixOS flake";
+  description = "Nixos config flake";
+
   inputs = {
-    nixpkgs-small.url = "github:NixOs/nixpkgs/nixos-unstable-small";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-22.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    webcord.url = "github:fufexan/webcord-flake"; # foss discord
-
-    tex2nix.url = "github:Mic92/tex2nix";
-    zig-overlay.url = "github:mitchellh/zig-overlay";
-    copilot-el = {
-      url = "github:zerolfx/copilot.el";
-      flake = false;
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
-    flake-parts.url = "github:hercules-ci/flake-parts";
   };
-  outputs = {
-    self,
-    nixpkgs,
-    nixpkgs-small,
-    nixpkgs-stable,
-    home-manager,
-    webcord,
-    tex2nix,
-    flake-parts,
-    zig-overlay,
-    ...
-  } @ inputs: let
-    system = "x86_64-linux";
-    lib = nixpkgs.lib;
-    overlays = [
-      zig-overlay.overlays.default
 
-      # This overlay exists to fix the ABI version of tree-sitter-python
-      (final: prev: {
-        tree-sitter-grammars =
-          prev.tree-sitter-grammars
-          // {
-            tree-sitter-python = prev.tree-sitter-grammars.tree-sitter-python.overrideAttrs (_: {
-              nativeBuildInputs = [final.nodejs final.tree-sitter];
-              configurePhase = ''
-                tree-sitter generate --abi 13 src/grammar.json
-              '';
-            });
-          };
-      })
-    ];
-  in
-    inputs.flake-parts.lib.mkFlake {inherit inputs;} {
-      systems = [system];
-      perSystem = {pkgs, ...}: {
-        formatter = pkgs.alejandra;
-      };
-      flake = {
-        nixosConfigurations = {
-          server = lib.nixosSystem {
-            inherit system;
-
-            modules = [
-              ./hosts/server/configuration.nix
-            ];
-          };
-
-          liveISO = lib.nixosSystem {
-            inherit system;
-
-            modules = [
-              ./hosts/iso.nix
-            ];
-          };
-
-          desktop = lib.nixosSystem {
-            inherit system;
-            modules = [
-              ./hosts/desktop/configuration.nix
-
-              home-manager.nixosModules.home-manager
-              {
-                nixpkgs.overlays = overlays;
-                home-manager = {
-                  useGlobalPkgs = true;
-                  useUserPackages = true;
-                  users.josh = import ./home-manager/users/josh.nix;
-                };
-              }
-            ];
-          };
-          laptop = lib.nixosSystem {
-            inherit system;
-            modules = [
-              ./hosts/laptop/configuration.nix
-
-              home-manager.nixosModules.home-manager
-              {
-                nixpkgs.overlays = overlays;
-                home-manager = {
-                  useGlobalPkgs = true;
-                  useUserPackages = true;
-                  users.josh = import ./home-manager/users/josh.nix;
-                };
-              }
-            ];
-          };
+  outputs = { self, nixpkgs, ... }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+    
+      nixosConfigurations.default = nixpkgs.lib.nixosSystem {
+          specialArgs = {inherit inputs;};
+          modules = [ 
+            ./configuration.nix
+            inputs.home-manager.nixosModules.default
+          ];
         };
-      };
+
     };
 }
