ca841352ada7722558ef04c9ebc5e89b7d0ec4b5
Nick <nlampri@gmail.com>
2024-10-07 22:00:28 +0300

    save

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -189,7 +189,8 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  #vi
+  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
   zsh-fzf-history-search
   vscode
