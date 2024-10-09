73caf375f78a8e484640e09caefed1358319986a
nick <nlampri@gmail.com>
2024-10-09 16:44:22 +0300

    save

--- rust.nix
+++ rust.nix
@@ -2,9 +2,9 @@
 { pkgs ? import <nixpkgs> { } }:
 pkgs.mkShell {
   buildInputs = with pkgs; [
+   openblas
    cmake
-   #cargo
-   #rustc
+   gfortran
    python311
    python311Packages.ipython
    python311Packages.matplotlib
